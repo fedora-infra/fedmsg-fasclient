61fcca809b31372bb5e793359df243cff5ee23cf
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-08-08 15:30:17 +0200

    Add the setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='fedmsg_fasclient',
+    version='0.1',
+    description='A fedmsg consumer that runs the fasClient based on fedmsg FAS messages',
+    license="LGPLv2+",
+    author='Janez Nemanič, Ralph Bean and Pierre-Yves Chibon',
+    author_email='admin@fedoraproject.org',
+    url='https://github.com/fedora-infra/fedmsg-fasclient',
+    install_requires=["fedmsg"],
+    packages=[],
+    py_modules=['fedmsg_fasclient'],
+    entry_points="""
+    [moksha.consumer]
+    fedmsg_fasclient = fedmsg_fasclient:FasClientConsumer
+    """,
+)
